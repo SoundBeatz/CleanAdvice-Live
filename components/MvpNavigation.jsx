048c9388e224fbaa3d252966e18b81373a8f7f35
SoundBeatz <info@u360.nl>
2026-06-30 21:41:20 +0200

    Add test room to MVP navigation

--- components/MvpNavigation.jsx
+++ components/MvpNavigation.jsx
@@ -2,6 +2,7 @@ export default function MvpNavigation() {
   return (
     <div className="actions">
       <a className="button ghost" href="/">Home</a>
+      <a className="button ghost" href="/test-room">Test Room</a>
       <a className="button ghost" href="/status">Status</a>
       <a className="button ghost" href="/test">API test</a>
       <a className="button ghost" href="/api/health">Health</a>
