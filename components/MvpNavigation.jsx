6abd78b300406d34358c0b0e812f295bb6555a2a
SoundBeatz <info@u360.nl>
2026-06-30 21:49:16 +0200

    Add quote and service to navigation

--- components/MvpNavigation.jsx
+++ components/MvpNavigation.jsx
@@ -2,6 +2,8 @@ export default function MvpNavigation() {
   return (
     <div className="actions">
       <a className="button ghost" href="/">Home</a>
+      <a className="button ghost" href="/quote">Quote</a>
+      <a className="button ghost" href="/service">Service</a>
       <a className="button ghost" href="/test-room">Test Room</a>
       <a className="button ghost" href="/status">Status</a>
       <a className="button ghost" href="/test">API test</a>
